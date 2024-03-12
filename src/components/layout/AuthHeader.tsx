bf334de5e95e46e399eb9ae1fceab9cb47dc2217
fikasp <fikasp@gmail.com>
2024-03-12 22:46:20 +0100

    Fix logo svg

--- src/components/layout/AuthHeader.tsx
+++ src/components/layout/AuthHeader.tsx
@@ -1,10 +1,10 @@
-import { ReactSVG } from 'react-svg'
+import Image from 'next/image'
 
 export default function AuthHeader() {
 
   return (
-    <header className="p-3 bg-white text-black">
-      <ReactSVG src="/assets/jointCare-logo.svg" />
+    <header className="flex-center p-3 bg-white text-black">
+      <Image src="/assets/jointCare-logo.svg" width={100} height={50} alt="logo" />
     </header>
   )
 }
