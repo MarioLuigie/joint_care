345a8706696e8bf5349f141ee1565d31a7d76bf5
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-03-12 14:12:34 +0100

    Add Shadcn-ui

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+	return (
+		<div className="flex-center">
+			<h1>DASHBOARD</h1>
+		</div>
+	)
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,86 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+import plugin from 'tailwindcss/plugin'
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+const config = {
+	darkMode: ['class'],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	prefix: '',
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px',
+			},
+		},
+
+		extend: {
+			colors: {
+				blue: {
+					DEFAULT: colors.blue[500],
+				},
+				orange: {
+					DEFAULT: colors.orange[500],
+				},
+			},
+			fontFamily: {
+				sans: ['Verdana', 'sans-serif'],
+				serif: ['Times New Roman', 'serif'],
+				poppins: ['var(--font-poppins)'],
+				roboto: ['var(--font-roboto)'],
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' },
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+			},
+		},
+	},
+	plugins: [
+		require('tailwindcss-animate'),
+		plugin(({ addUtilities }) => {
+			addUtilities({
+				'.flex-start': {
+					display: 'flex',
+					justifyContent: 'start',
+					alignItems: 'center',
+				},
+				'.flex-center': {
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+				},
+				'.flex-between': {
+					display: 'flex',
+					justifyContent: 'space-between',
+					alignItems: 'center',
+				},
+				'.flex-end': {
+					display: 'flex',
+					justifyContent: 'flex-end',
+					alignItems: 'center',
+				},
+				'.grid-auto-300': {
+					gridTemplateColumns: 'repeat(auto-fill, minmax(300px,1fr));',
+				},
+			})
+		}),
+	],
+} satisfies Config
+
+export default config
