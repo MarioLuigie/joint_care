efdac5206794d104241235a38d5fdbadc9a7e81b
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-03-20 21:31:04 +0100

    Feat: Redirect from registration to login or forgot-password route

--- src/components/content/auth/Registration.tsx
+++ src/components/content/auth/Registration.tsx
@@ -137,11 +137,11 @@ export default function Registration() {
 						<Warning>
 							<p>Konto z tym adresem e-mail jest już zarejestrowane.</p>
 							<div className='flex items-center gap-2 text-jc-text1'>
-								<Link href="#" className="jc-warning-link underline">
+								<Link href="/auth/login" className="jc-warning-link underline">
 									Zaloguj się
 								</Link>
 								<p className="jc-warning-link">lub</p>
-								<Link href="#" className="jc-warning-link underline">
+								<Link href="/auth/forgot-password" className="jc-warning-link underline">
 									Przypomnij hasło
 								</Link>
 							</div>
