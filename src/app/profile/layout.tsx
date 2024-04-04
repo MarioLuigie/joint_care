537fe81672337317847000c8fdd5a58021fff5b8
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-04-04 11:23:15 +0200

    Feat: Create profile section routing

---
+++ src/app/profile/layout.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/layout/Header'
+import ProfileMain from '@/components/layout/profile/ProfileMain'
+import Footer from '@/components/layout/Footer'
+
+export default function ProfileLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Header />
+			<ProfileMain>{children}</ProfileMain>
+			<Footer />
+		</div>
+	)
+}
---
+++ src/app/profile/my-profile/page.tsx
@@ -0,0 +1,7 @@
+export default function MyProfilePage() {
+	return (
+		<div className="flex justify-center items-start flex-wrap gap-6">
+			MY PROFILE PAGE
+		</div>
+	)
+}
